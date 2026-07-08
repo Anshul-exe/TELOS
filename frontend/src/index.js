2dc6671950a73929caaa27ba088e2835872ada05
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2026-07-09 02:46:47 +0530

    chore: index updates

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <div className="app-wrapper">
-            <App />
-        </div>
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <div className="app-wrapper">
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
